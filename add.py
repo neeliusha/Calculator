37d5a158e0c3e52b80c9601ba0ec895bd2fd0c23
neeliusha <167849418+neeliusha@users.noreply.github.com>
2025-12-11 21:32:41 +0530

    Update add.py
    Commiting after changing to dynamic user input

--- add.py
+++ add.py
@@ -1,8 +1,16 @@
 # This program adds two numbers with static input
 
+'''
 num1 = 1.5
 num2 = 2.5
 sum = num1 + num2
+'''
+# commenting static input program and changed to dynamic user input program
+
+a = input("First number: ")
+b = input("Second number: ")
+res = float(a) + float(b)
+
 
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum)
